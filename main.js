d366ea8207a5ee4789b676b9581bbf6e8aa76f36
Jason Ross <jaross@redhat.com>
2021-12-07 15:43:48 -0600

    ignore ssl

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 // Modules to control application life and create native browser window
 const {app, BrowserWindow} = require('electron')
+
+app.commandLine.appendSwitch('ignore-certificate-errors')
+
 const path = require('path')
 
 function createWindow () {
